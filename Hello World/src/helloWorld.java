8f580a8e3db756038509816db0aff96577842064
user <user@RUSH-8N0GHMDPE0>
2019-01-25 14:15:54 -0600

    Committing testing stuff

--- Hello World/src/helloWorld.java
+++ Hello World/src/helloWorld.java
@@ -4,8 +4,6 @@ public class helloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
-		System.out.println("hello world 2");
-		System.out.println("testing out github");
 	}
 
 }
